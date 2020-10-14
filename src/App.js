33aee3d1f059be39cdcbded9a1177609fd4715c2
IZUNA894 <jainsanyam1261@gmail.com>
2020-10-15 00:00:57 +0530

    second section completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
-import IndiaMap2 from "./components/k-file1";
+import SecondSection from "./components/secondSection";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="row">
-        <div className="col s5 ">
-          <div className="card">
-            <h3>India</h3>
-            <div className="card-content" style={{ height: "80vh" }}>
-              <IndiaMap2 />
-            </div>
-          </div>
-        </div>
-      </div>
+
+      {/* middle section */}
+      <SecondSection />
     </div>
   );
 }
